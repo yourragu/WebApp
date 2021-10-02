6ea4f3b078b7ecada8c9e913bb1f99e83135f942
TestLeaf <babu@testleaf.com>
2021-10-02 15:17:08 +0530

    Changed the port number

--- StopApp.bat
+++ StopApp.bat
@@ -1,1 +1,1 @@
-for /f "tokens=5" %%a in ('netstat -aon ^| find ":9001" ^| find "LISTENING"') do taskkill /f /pid %%a
+for /f "tokens=5" %%a in ('netstat -aon ^| find ":9002" ^| find "LISTENING"') do taskkill /f /pid %%a
